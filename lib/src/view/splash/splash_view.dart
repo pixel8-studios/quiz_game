ae3ce69eb3ef36e72c74cc9feeaa8f907a51306d
Medeiros <hsymetry@gmail.com>
2022-12-06 23:57:36 -0300

    Add login page

--- lib/src/view/splash/splash_view.dart
+++ lib/src/view/splash/splash_view.dart
@@ -1,14 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/src/view/login/login_view.dart';
 
 class SplashView extends StatelessWidget {
+  const SplashView({super.key});
 
-  const SplashView({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      duration: 2000,
+      splash: SizedBox(
+        height: MediaQuery.of(context).size.height * .6,
+        child: Image.asset(
+          'assets/images/ximioni_logo.png',
+        ),
+      ),
+      nextScreen: LoginView(),
+      splashTransition: SplashTransition.fadeTransition,
+      backgroundColor: Colors.white,
+    );
   }
 }
